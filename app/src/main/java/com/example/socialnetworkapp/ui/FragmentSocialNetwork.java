d46164380fc83a044481d029266d3af42afe8c41
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-08-03 07:19:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/socialnetworkapp/ui/FragmentSocialNetwork.java
@@ -0,0 +1,52 @@
+package com.example.socialnetworkapp.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.socialnetworkapp.R;
+
+
+public class FragmentSocialNetwork extends Fragment {
+
+    public static FragmentSocialNetwork newInstance() {
+        //Bundle args = new Bundle();
+        FragmentSocialNetwork fragment = new FragmentSocialNetwork();
+        //fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_social_network, container, false);
+        RecyclerView recyclerView = v.findViewById(R.id.recycler_view_lines);
+        String[] data = getResources().getStringArray(R.array.titles_array);
+        initRecyclerView(recyclerView, data);
+        return v;
+    }
+
+    private void initRecyclerView(RecyclerView recyclerView, String[] data) {
+        // Для начала надо найти элемент RecyclerView. Поскольку у нас все элементыспискаодинаковые по
+        // размеру, стоит установить параметр recyclerView.setHasFixedSize(true);. Это ускорит
+        // обработку.
+        // Эта установка служит для повышения производительности системы
+        recyclerView.setHasFixedSize(true);
+
+        // Далее создаём менеджер, пусть это будет линейный менеджер.
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Затем создаём адаптер и передаём ему данные в виде массива.
+        SocialNetworkAdapter adapter = new SocialNetworkAdapter(data);
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/socialnetworkapp/ui/SocialNetworkAdapter.java
@@ -0,0 +1,77 @@
+package com.example.socialnetworkapp.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.socialnetworkapp.R;
+
+public class SocialNetworkAdapter extends RecyclerView.Adapter<SocialNetworkAdapter.ViewHolder> {
+
+    private String[] dataSource;
+
+    // В конструкторе адаптера передаём источник данных. По нему будем строить список. Пока для
+    // простоты это будет массив строк, но также можно передавать список со сложной структурой,
+    // считываемый из базы данных.
+    public SocialNetworkAdapter(String[] dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    // Создать новый элемент пользовательского интерфейса
+    // Запускается менеджером
+    @NonNull
+    @Override
+    public SocialNetworkAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        // Создаём новый элемент пользовательского интерфейса
+        // Через Inflater
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        // Здесь можно установить всякие параметры
+
+        return new ViewHolder(v);
+    }
+
+    // Заменить данные в пользовательском интерфейсе
+    // Вызывается менеджером
+    @Override
+    public void onBindViewHolder(@NonNull SocialNetworkAdapter.ViewHolder holder, int position) {
+        // Получить элемент из источника данных (БД, интернет...)
+        // Вынести на экран, используя ViewHolder
+        holder.getTextView().setText(dataSource[position]);
+
+    }
+
+    // Вернуть размер данных, вызывается менеджером
+    @Override
+    public int getItemCount() {
+        return dataSource.length;
+    }
+
+    // Этот класс хранит связь между данными и элементами View
+    // Сложные данные могут потребовать несколько View на один пункт списка
+    // Класс ViewHolder хранит в себе пользовательские элементы, которые передаются в конструктор,
+    // чтобы позже их заполнить данными. Поскольку это RecyclerView, то есть он умеет
+    // переиспользовать ViewHolder, беря уже неиспользуемые, то заполнение данными элементов
+    // ViewHolder отделяют от создания ViewHolder. Создаём поле textView, реализуем геттер.
+    // Присваиваем это поле в конструкторе.
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textView = (TextView) itemView;
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+}
